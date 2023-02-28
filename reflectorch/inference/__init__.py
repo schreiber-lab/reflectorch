d78d3764b88a2ac015d9b3c2d1acf12733513b3e
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2023-02-28 11:09:37 +0100

    add __all__

---
+++ reflectorch/inference/__init__.py
@@ -0,0 +1,11 @@
+from reflectorch.inference.inference_model import InferenceModel
+from reflectorch.inference.preprocess_exp import (
+    StandardPreprocessing,
+    standard_preprocessing,
+)
+
+__all__ = [
+    "InferenceModel",
+    "StandardPreprocessing",
+    "standard_preprocessing",
+]
