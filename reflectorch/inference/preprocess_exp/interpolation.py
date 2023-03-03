847419c6a3711040262e727d0d4259c50ef8a85d
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2023-03-03 12:37:08 +0100

    fix zero reflectivity

--- reflectorch/inference/preprocess_exp/interpolation.py
+++ reflectorch/inference/preprocess_exp/interpolation.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 
-def interp_reflectivity(q_interp, q, reflectivity):
+def interp_reflectivity(q_interp, q, reflectivity, min_value: float = 1e-10):
     """Interpolate data on a base10 logarithmic scale."""
-    return 10 ** np.interp(q_interp, q, np.log10(reflectivity))
+    return 10 ** np.interp(q_interp, q, np.log10(np.clip(reflectivity, min_value, None)))
