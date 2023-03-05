ed57b428d15b597773172595fb62f18f08b47083
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2023-03-05 14:54:37 +0100

    added kinematical_approximation

--- reflectorch/data_generation/reflectivity.py
+++ reflectorch/data_generation/reflectivity.py
@@ -175,6 +175,51 @@ def abeles_fast(
     return r
 
 
+def kinematical_approximation(
+        q: Tensor,
+        thickness: Tensor,
+        roughness: Tensor,
+        sld: Tensor,
+):
+    c_dtype = torch.complex128 if q.dtype is torch.float64 else torch.complex64
+
+    batch_size, num_layers = thickness.shape
+
+    q = q.to(c_dtype)
+
+    if q.dim() == 1:
+        q.unsqueeze_(0)
+
+    if q.dim() == 2:
+        q.unsqueeze_(-1)
+
+    sld = sld * 1e-6 + 1e-30j
+
+    drho = torch.cat([sld[..., 0][..., None], sld[..., 1:] - sld[..., :-1]], -1)[:, None]
+    thickness = torch.cumsum(torch.cat([torch.zeros(batch_size, 1).to(thickness), thickness], -1), -1)[:, None]
+    roughness = roughness[:, None]
+
+    r = (drho * torch.exp(- (roughness * q) ** 2 / 2 + 1j * (q * thickness))).sum(-1).abs().float() ** 2
+
+    substrate_sld = sld[:, -1:]
+
+    rf = _get_resnel_reflectivity(q, substrate_sld[:, None])
+
+    r = torch.clamp_max_(r * rf / substrate_sld.real ** 2, 1.)
+
+    return r
+
+
+def _get_resnel_reflectivity(q, substrate_slds):
+    _RE_CONST = 0.28174103675406496
+
+    q_c = torch.sqrt(substrate_slds + 0j) / _RE_CONST * 2
+    q_prime = torch.sqrt(q ** 2 - q_c ** 2 + 0j)
+    r_f = ((q - q_prime) / (q + q_prime)).abs().float() ** 2
+
+    return r_f.squeeze(-1)
+
+
 def abeles_np(
         q: np.ndarray,
         thickness: np.ndarray,
@@ -240,6 +285,7 @@ def abeles_np(
         r = r[0]
 
     return r
+
 
 def _get_relative_k_z(k_z0, scattering_length_density):
     return torch.sqrt(k_z0 ** 2 - 4 * pi * scattering_length_density)
