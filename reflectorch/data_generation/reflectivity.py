48b5dae6e94f2b4b2f23aea96dc2e3e5397fbd75
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2023-03-05 15:23:39 +0100

    added kinematical_approximation_np

--- reflectorch/data_generation/reflectivity.py
+++ reflectorch/data_generation/reflectivity.py
@@ -220,6 +220,56 @@ def _get_resnel_reflectivity(q, substrate_slds):
     return r_f.squeeze(-1)
 
 
+def kinematical_approximation_np(
+        q: np.ndarray,
+        thickness: np.ndarray,
+        roughness: np.ndarray,
+        sld: np.ndarray,
+):
+
+    if q.ndim == thickness.ndim == roughness.ndim == sld.ndim == 1:
+        zero_batch = True
+    else:
+        zero_batch = False
+
+    thickness = np.atleast_2d(thickness)
+    roughness = np.atleast_2d(roughness)
+    sld = np.atleast_2d(sld) * 1e-6 + 1e-30j
+    substrate_sld = sld[:, -1:]
+
+    batch_size, num_layers = thickness.shape
+
+    if q.ndim == 1:
+        q = q[None]
+
+    if q.ndim == 2:
+        q = q[..., None]
+
+    drho = np.concatenate([sld[..., 0][..., None], sld[..., 1:] - sld[..., :-1]], -1)[:, None]
+    thickness = np.cumsum(np.concatenate([np.zeros((batch_size, 1)), thickness], -1), -1)[:, None]
+    roughness = roughness[:, None]
+
+    r = np.abs((drho * np.exp(- (roughness * q) ** 2 / 2 + 1j * (q * thickness))).sum(-1)).astype(float) ** 2
+
+    rf = _get_resnel_reflectivity_np(q, substrate_sld[:, None])
+
+    r = np.clip(r * rf / np.real(substrate_sld) ** 2, None, 1.)
+
+    if zero_batch:
+        r = r[0]
+
+    return r
+
+
+def _get_resnel_reflectivity_np(q, substrate_slds):
+    _RE_CONST = 0.28174103675406496
+
+    q_c = np.sqrt(substrate_slds + 0j) / _RE_CONST * 2
+    q_prime = np.sqrt(q ** 2 - q_c ** 2 + 0j)
+    r_f = np.abs((q - q_prime) / (q + q_prime)).astype(float) ** 2
+
+    return r_f[..., 0]
+
 def abeles_np(
         q: np.ndarray,
         thickness: np.ndarray,
