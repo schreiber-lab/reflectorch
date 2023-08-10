8e20f7fd8fb94157f1f6265279ae33962277a7de
valentinsingularity <60343516+valentinsingularity@users.noreply.github.com>
2023-08-10 11:35:34 +0200

    Add files via upload

--- reflectorch/models/encoders/conv_fc_encoder.py
+++ reflectorch/models/encoders/conv_fc_encoder.py
@@ -3,8 +3,9 @@
 import math
 from torch import nn, cat, split
 
-from reflectorch.models.encoders.residual_net import ResidualNet
+from reflectorch.models.encoders.residual_net import ResidualNet, ResidualNet_FiLM
 from reflectorch.models.encoders.conv_encoder import ConvEncoder
+from reflectorch.models.encoders.fno import FNO_Enc
 from reflectorch.models.utils import activation_by_name
 
 
@@ -46,7 +47,7 @@ class ConvFCEncoder(nn.Module):
         x = cat([self.conv(x), self.fc(x)], dim=-1)
         x = self.cat_fc(x)
         return x
-
+    
 
 class SeqConvFCEncoder(nn.Module):
     def __init__(self,
@@ -206,6 +207,210 @@ class SubPriorConvFCEncoder(nn.Module):
         if self.pass_bounds:
             x = cat([x, bounds], dim=-1)
         return x
+    
+    
+class SubPriorConvFCEncoder_V2(nn.Module): #only the embedding and the prior are input to the MLP (the reflectivity curve is not concatenated)
+    def __init__(self,
+                 in_channels: int = 1,
+                 hidden_dims: tuple = (16, 32, 64),
+                 latent_dim: int = 64,
+                 conv_latent_dim: int = 64,
+                 avpool: int = 8,
+                 use_batch_norm: bool = False,
+                 in_features: int = 64,
+                 prior_in_features: int = 16,
+                 hidden_features: int = 64,
+                 num_blocks: int = 3,
+                 conv_activation: str = 'gelu',
+                 fc_activation: str = 'gelu',
+                 use_selu_init: bool = False,
+                 pass_bounds: bool = False,
+                 sigmas_input: bool = False,
+                 pretrained_conv: str = None,
+                 ):
+        super().__init__()
+        self.pass_bounds = pass_bounds
+        self.sigmas_input = sigmas_input
+        self.in_channels = in_channels
+
+        self.conv = ConvEncoder(
+            in_channels=in_channels, hidden_dims=hidden_dims, latent_dim=conv_latent_dim, avpool=avpool,
+            use_batch_norm=use_batch_norm, activation=activation_by_name(conv_activation)
+        )
+        
+        self.prior_in_features = prior_in_features
+        fc_in_dim = conv_latent_dim + prior_in_features
+
+        if self.sigmas_input:
+            fc_in_dim += in_features
+
+        self.fc = ResidualNet(
+            fc_in_dim,
+            latent_dim,
+            hidden_features,
+            activation=activation_by_name(fc_activation),
+            num_blocks=num_blocks,
+            use_batch_norm=use_batch_norm
+        )
+
+        if use_selu_init and conv_activation == 'selu':
+            self.conv.apply(selu_init)
+
+        if use_selu_init and fc_activation == 'selu':
+            self.fc.apply(selu_init)
+
+        if pretrained_conv:
+            self.conv.load_weights(pretrained_conv)
+
+
+    def forward(self, x, q_values=None):
+        if isinstance(x, dict):
+            curves, bounds = x['scaled_curves'], x['scaled_bounds']
+        else:
+            #curves, bounds = split(x, self._split, dim=-1)
+            curves, bounds = split(x, [x.shape[-1]-self.prior_in_features, self.prior_in_features], dim=-1)
+            
+        if q_values is not None:
+            curves = cat([curves[:, None, :], q_values.float()[:, None, :]], dim=1)
+
+        x = cat([self.conv(curves), bounds], dim=-1)
+        x = self.fc(x)
+        if self.pass_bounds:
+            x = cat([x, bounds], dim=-1)
+        return x
+    
+    
+class SubPriorConvFCEncoder_FiLM(nn.Module):
+    def __init__(self,
+                 in_channels: int = 1,
+                 hidden_dims: tuple = (16, 32, 64),
+                 latent_dim: int = 64,
+                 conv_latent_dim: int = 64,
+                 avpool: int = 8,
+                 use_batch_norm: bool = False,
+                 in_features: int = 64,
+                 prior_in_features: int = 16,
+                 hidden_features: int = 64,
+                 num_blocks: int = 3,
+                 conv_activation: str = 'gelu',
+                 fc_activation: str = 'gelu',
+                 use_selu_init: bool = False,
+                 pass_bounds: bool = False,
+                 pretrained_conv: str = None,
+                 ):
+        super().__init__()
+        self.pass_bounds = pass_bounds
+        self.in_channels = in_channels
+
+        self.conv = ConvEncoder(
+            in_channels=in_channels, hidden_dims=hidden_dims, latent_dim=conv_latent_dim, avpool=avpool,
+            use_batch_norm=use_batch_norm, activation=activation_by_name(conv_activation)
+        )
+        
+        self.prior_in_features = prior_in_features
+        fc_in_dim = conv_latent_dim
+
+        self.fc = ResidualNet_FiLM(
+            fc_in_dim,
+            prior_in_features,
+            latent_dim,
+            hidden_features,
+            activation=activation_by_name(fc_activation),
+            num_blocks=num_blocks,
+            use_batch_norm=use_batch_norm
+        )
+
+        if use_selu_init and conv_activation == 'selu':
+            self.conv.apply(selu_init)
+
+        if use_selu_init and fc_activation == 'selu':
+            self.fc.apply(selu_init)
+
+        if pretrained_conv:
+            self.conv.load_weights(pretrained_conv)
+
+
+    def forward(self, x, q_values=None):
+        if isinstance(x, dict):
+            curves, bounds = x['scaled_curves'], x['scaled_bounds']
+        else:
+            #curves, bounds = split(x, self._split, dim=-1)
+            curves, bounds = split(x, [x.shape[-1]-self.prior_in_features, self.prior_in_features], dim=-1)
+            
+        if q_values is not None:
+            curves = cat([curves[:, None, :], q_values.float()[:, None, :]], dim=1)
+
+        x = self.conv(curves)
+        x = self.fc(x, bounds)
+        if self.pass_bounds:
+            x = cat([x, bounds], dim=-1)
+        return x
+    
+    
+class SubPrior_FNO_MLP(nn.Module):
+    def __init__(self,
+                 latent_dim: int = 8, #output_dim
+                 hidden_features: int = 64, #hidden layer width mlp
+                 num_blocks: int = 3, #res_mlp blocks
+                 prior_in_features: int = 16,
+                 use_batch_norm: bool = False,
+                 fc_activation: str = 'gelu',
+                 
+                 ch_in: int = 2,
+                 dim_embedding: int = 128,
+                 modes: int = 16,
+                 width_fno: int = 64,
+                 fno_activation: str = 'gelu',
+                 n_fno_blocks : int = 6,
+                 
+                 use_selu_init: bool = False,
+                 pass_bounds: bool = False,
+                 ):
+        super().__init__()
+        self.pass_bounds = pass_bounds
+
+        self.fno_enc = FNO_Enc(
+            ch_in=ch_in, dim_embedding=dim_embedding, modes=modes, width_fno=width_fno, 
+            n_fno_blocks=n_fno_blocks, activation=activation_by_name(fno_activation)
+        )
+
+        self.prior_in_features = prior_in_features
+        fc_in_dim = dim_embedding + prior_in_features
+
+        self.fc = ResidualNet(
+            fc_in_dim,
+            latent_dim,
+            hidden_features,
+            activation=activation_by_name(fc_activation),
+            num_blocks=num_blocks,
+            use_batch_norm=use_batch_norm
+        )
+
+        if use_selu_init and conv_activation == 'selu':
+            self.fno_enc.apply(selu_init)
+
+        if use_selu_init and fc_activation == 'selu':
+            self.fc.apply(selu_init)
+
+            
+    def forward(self, x, q_values=None):
+        if isinstance(x, dict):
+            curves, bounds = x['scaled_curves'], x['scaled_bounds']
+        else:
+            curves, bounds = split(x, [x.shape[-1]-self.prior_in_features, self.prior_in_features], dim=-1)
+            
+        if q_values is not None:
+            q_input = 4 * q_values.float()[:, None, :] - 1
+            curves = cat([curves[:, None, :], q_input], dim=1)
+        else:
+            curves = curves[:, None, :]
+
+        x = cat([self.fno_enc(curves), bounds], dim=-1)
+        x = self.fc(x)
+        if self.pass_bounds:
+            x = cat([x, bounds], dim=-1)
+        return x
+
 
 
 def selu_init(m):
