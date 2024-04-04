e49e40ec96414f130d58bc5370665bac85853d9e
Valentin Munteanu <60343516+valentinsingularity@users.noreply.github.com>
2024-04-04 13:30:55 +0200

    added docstring

--- reflectorch/models/utils.py
+++ reflectorch/models/utils.py
@@ -14,6 +14,14 @@ ACTIVATIONS = {
 
 
 def activation_by_name(name):
+    """returns an activation function module corresponding to its name
+
+    Args:
+        name (str): string denoting the activation function ('relu', 'lrelu', 'gelu', 'selu', 'elu', 'sigmoid', 'silu', 'mish')
+
+    Returns:
+        nn.Module: Pytorch activation function module
+    """
     if not isinstance(name, str):
         return name
     try:
