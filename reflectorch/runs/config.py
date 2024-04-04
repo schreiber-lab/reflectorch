f561575ffc530f2d444caab6975a9a186e8cae24
Valentin Munteanu <60343516+valentinsingularity@users.noreply.github.com>
2024-04-04 11:57:18 +0200

    added docstring

--- reflectorch/runs/config.py
+++ reflectorch/runs/config.py
@@ -10,10 +10,19 @@ from reflectorch.paths import CONFIG_DIR
 
 
 def load_config(config_name: str) -> dict:
+    """Initializes a configuration dictionary from a configuration file in the configuration directory
+
+    Args:
+        config_name (str): name of the configuration file
+
+    Returns:
+        dict: the configuration dictionary
+    """
     if not config_name.endswith('.yaml'):
         config_name = f'{config_name}.yaml'
     path = CONFIG_DIR / config_name
     with open(path, 'r') as f:
         config = yaml.safe_load(f)
     config['config_path'] = str(path.absolute())
+    
     return config
