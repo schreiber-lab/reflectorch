c69c5919e351de97a8d7937629eed8e79d7648d6
Valentin Munteanu <60343516+valentinsingularity@users.noreply.github.com>
2024-04-15 16:10:42 +0200

    added functionaliy to load the config from a directory different than the default

--- reflectorch/runs/config.py
+++ reflectorch/runs/config.py
@@ -6,11 +6,12 @@
 
 import yaml
 
+from pathlib import Path
 from reflectorch.paths import CONFIG_DIR
 
 
-def load_config(config_name: str) -> dict:
-    """Initializes a configuration dictionary from a YAML configuration file located in the configuration directory
+def load_config(config_name: str, config_dir: str = None) -> dict:
+    """Loads a configuration dictionary from a YAML configuration file located in the configuration directory
 
     Args:
         config_name (str): name of the YAML configuration file
@@ -20,7 +21,8 @@ def load_config(config_name: str) -> dict:
     """
     if not config_name.endswith('.yaml'):
         config_name = f'{config_name}.yaml'
-    path = CONFIG_DIR / config_name
+    config_dir = Path(config_dir) if config_dir else CONFIG_DIR
+    path = config_dir / config_name
     with open(path, 'r') as f:
         config = yaml.safe_load(f)
     config['config_path'] = str(path.absolute())
