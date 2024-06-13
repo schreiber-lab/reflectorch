4f48912450e7fef211bea7f54c1bac5e1216c6cf
Valentin Munteanu <60343516+valentinsingularity@users.noreply.github.com>
2024-06-13 12:50:23 +0200

    inference model

--- reflectorch/inference/inference_model.py
+++ reflectorch/inference/inference_model.py
@@ -23,26 +23,41 @@ from reflectorch.inference.record_time import print_time
 from reflectorch.utils import to_t
 
 class EasyInferenceModel(object):
-    def __init__(self, config_name: str = None, config_dir:str = None, trainer: PointEstimatorTrainer = None, preprocessing_parameters: dict = None, device='cuda'):
+    """Facilitates the inference process using pretrained models
+    
+    Args:
+        config_name (str, optional): the name of the configuration file used to initialize the model. Defaults to None.
+        config_dir (str, optional): path to the directory containing the configuration file. Defaults to None.
+        model_path (str, optional): path to the saved weights of the neural network. Defaults to None.
+        trainer (PointEstimatorTrainer, optional): if provided, this trainer instance is used instead of initializing from the configuration file . Defaults to None.
+        preprocessing_parameters (dict, optional): dictionary of parameters for preprocessing raw data. Defaults to None.
+        device (str, optional): the Pytorch device ('cuda' or 'cpu'). Defaults to 'cuda'.
+    """
+    def __init__(self, config_name: str = None, config_dir: str = None, model_path: str = None, trainer: PointEstimatorTrainer = None, preprocessing_parameters: dict = None, device='cuda'):
         self.config_name = config_name
         self.config_dir = config_dir
+        self.model_path = model_path
         self.trainer = trainer
         self.device = device
         self.preprocessing = StandardPreprocessing(**(preprocessing_parameters or {}))
 
         if trainer is None and self.config_name is not None:
-            self.load_model(self.config_name, self.config_dir)
+            self.load_model(self.config_name, self.config_dir, self.model_path)
 
-    def load_model(self, config_name: str, config_dir: str) -> None:
+    def load_model(self, config_name: str, config_dir: str, model_path: str) -> None:
         print(f"Loading model {config_name}")
         if self.config_name == config_name and self.trainer is not None:
             return
         
         self.config_name = config_name
         self.config_dir = config_dir
-        self.trainer = get_trainer_by_name(config_name=config_name, config_dir=config_dir, load_weights=True)
+        self.model_path = model_path
+        self.trainer = get_trainer_by_name(config_name=config_name, config_dir=config_dir, model_path=model_path, load_weights=True, inference_device = self.device)
         self.trainer.model.eval()
         print(f"Model {config_name} is loaded.")
+
+    def load_model_from_hugginface(self, config_name: str):
+        raise NotImplementedError
 
     def predict(self, reflectivity_curve: np.ndarray or Tensor, q_values: np.ndarray or Tensor, prior_bounds: List[Tuple]):
         scaled_curve = self._scale_curve(reflectivity_curve)
@@ -56,13 +71,14 @@ class EasyInferenceModel(object):
         with torch.no_grad():
             self.trainer.model.eval()
             if self.trainer.train_with_q_input:
-                scaled_predicted_params = self.trainer.model(scaled_curve_and_priors, q_values)
+                scaled_q = self.trainer.loader.q_generator.scale_q(q_values).float()
+                scaled_predicted_params = self.trainer.model(scaled_curve_and_priors, scaled_q)
             else:
                 scaled_predicted_params = self.trainer.model(scaled_curve_and_priors)
         
         restored_predictions = self.trainer.loader.prior_sampler.restore_params(torch.cat([scaled_predicted_params, scaled_prior_bounds], dim=-1))
 
-        return restored_predictions.parameters
+        return restored_predictions
     
     def predict_using_widget(self, reflectivity_curve: np.ndarray or Tensor, q_values: np.ndarray or Tensor):
 
@@ -107,195 +123,13 @@ class EasyInferenceModel(object):
             array_values = np.array(values)
             
             predicted_params = self.predict(reflectivity_curve=reflectivity_curve, q_values=q_values, prior_bounds=array_values)
-            print(f'The prediction is: {predicted_params.squeeze().cpu().numpy()}')
-            return predicted_params.squeeze().cpu().numpy()
+            print(f'The prediction is: ')
+            for l, p in zip(get_param_labels(2), predicted_params.parameters.squeeze().cpu().numpy()):
+                print(f'{l.ljust(14)} --> Predicted: {p:.2f}')
+
+            return predicted_params
 
         button.on_click(store_values)
-
-    def _scale_curve(self, curve: np.ndarray or Tensor):
-        if not isinstance(curve, Tensor):
-            curve = torch.from_numpy(curve).float()
-        curve = torch.atleast_2d(curve).to(self.device)
-        scaled_curve = self.trainer.loader.curves_scaler.scale(curve)
-        return scaled_curve
-    
-    def _scale_prior_bounds(self, prior_bounds: List[Tuple]):
-        prior_bounds = torch.tensor(prior_bounds)
-        prior_bounds = prior_bounds.to(self.device).T
-        min_bounds, max_bounds = prior_bounds[:, None]
-
-        scaled_bounds = torch.cat([
-            self.trainer.loader.prior_sampler.scale_bounds(min_bounds), 
-            self.trainer.loader.prior_sampler.scale_bounds(max_bounds)
-        ], -1)
-
-        return scaled_bounds.float()
-    
-        #inference_model = EasyInferenceModel(config_name='c1_trained')
-        #prior_bounds = [(300., 400.), (-10., 500.), (0., 60.), (0., 60.), (0., 60.), (-25., 25.), (-25., 25.), (0., 1.)]
-        #refl_curve = inference_model.trainer.loader.get_batch(1)['scaled_noisy_curves']
-        #refl_curve = inference_model.trainer.loader.curves_scaler.restore(refl_curve).squeeze().cpu().numpy()
-        #q_values = inference_model.trainer.loader.get_batch(1)['q_values'].squeeze().cpu().numpy()
-        #pred = inference_model.predict(refl_curve, q_values, prior_bounds)
-
-class InferenceModel(object):
-    def __init__(self, name: str = None, trainer: PointEstimatorTrainer = None, preprocessing_parameters: dict = None,
-                 num_sampling: int = 2 ** 13):
-        self.log = logging.getLogger(__name__)
-        self.model_name = name
-        self.trainer = trainer
-        self.q = None
-        self.preprocessing = StandardPreprocessing(**(preprocessing_parameters or {}))
-        self._sampling_num = num_sampling
-
-        if trainer is None and self.model_name is not None:
-            self.load_model(self.model_name)
-        elif trainer is not None:
-            self._set_trainer(trainer, preprocessing_parameters)
-
-    ### API methods ###
-
-    def load_model(self, name: str) -> None:
-        self.log.debug(f"loading model {name}")
-        if self.model_name == name and self.trainer is not None:
-            return
-        self.model_name = name
-        self._set_trainer(get_trainer_by_name(name))
-        self.log.info(f"Model {name} is loaded.")
-
-    def train_model(self, name: str):
-        self.model_name = name
-        self.trainer = train_from_config(load_config(name))
-
-    def set_preprocessing_parameters(self, **kwargs) -> None:
-        self.preprocessing.set_parameters(**kwargs)
-
-    def preprocess(self,
-                   intensity: np.ndarray,
-                   scattering_angle: np.ndarray,
-                   attenuation: np.ndarray,
-                   update_params: bool = False,
-                   **kwargs) -> dict:
-        if update_params:
-            self.preprocessing.set_parameters(**kwargs)
-        preprocessed_dict = self.preprocessing(intensity, scattering_angle, attenuation, **kwargs)
-        return preprocessed_dict
-
-    def predict(self,
-                intensity: np.ndarray,
-                scattering_angle: np.ndarray,
-                attenuation: np.ndarray,
-                priors: np.ndarray,
-                preprocessing_parameters: dict = None,
-                polish: bool = True,
-                use_sampler: bool = False,
-                use_q_shift: bool = True,
-                max_d_change: float = 5.,
-                fit_growth: bool = True,
-                ) -> dict:
-
-        with print_time("everything"):
-            with print_time("preprocess"):
-                preprocessed_dict = self.preprocess(
-                    intensity, scattering_angle, attenuation, **(preprocessing_parameters or {})
-                )
-
-            preprocessed_curve = preprocessed_dict["curve_interp"]
-            raw_curve, raw_q = preprocessed_dict["curve"], preprocessed_dict["q_values"]
-            q_ratio = preprocessed_dict["q_ratio"]
-
-            with print_time("predict_from_preprocessed_curve"):
-                preprocessed_dict.update(self.predict_from_preprocessed_curve(
-                    preprocessed_curve, priors, raw_curve=raw_curve, raw_q=raw_q, polish=polish, q_ratio=q_ratio,
-                    use_sampler=use_sampler, use_q_shift=use_q_shift, max_d_change=max_d_change,
-                    fit_growth=fit_growth,
-                ))
-
-            return preprocessed_dict
-
-    def predict_from_preprocessed_curve(self,
-                                        curve: np.ndarray,
-                                        priors: np.ndarray, *,
-                                        polish: bool = True,
-                                        raw_curve: np.ndarray = None,
-                                        raw_q: np.ndarray = None,
-                                        clip_prediction: bool = True,
-                                        q_ratio: float = 1.,
-                                        use_sampler: bool = False,
-                                        use_q_shift: bool = True,
-                                        max_d_change: float = 5.,
-                                        fit_growth: bool = True,
-                                        ) -> dict:
-
-        scaled_curve = self._scale_curve(curve)
-        scaled_bounds, min_bounds, max_bounds = self._scale_priors(priors, q_ratio)
-
-        if not use_q_shift:
-            predicted_params: UniformSubPriorParams = self._simple_prediction(scaled_curve, scaled_bounds)
-        else:
-            predicted_params: UniformSubPriorParams = self._qshift_prediction(curve, scaled_bounds)
-
-        if use_sampler:
-            predicted_params: UniformSubPriorParams = self._sampler_solution(
-                curve, predicted_params,
-            )
-
-        if clip_prediction:
-            predicted_params = self._prior_sampler.clamp_params(predicted_params)
-
-        if raw_curve is None:
-            raw_curve = curve
-        if raw_q is None:
-            raw_q = self.q.squeeze().cpu().numpy()
-            raw_q_t = self.q
-        else:
-            raw_q_t = torch.from_numpy(raw_q).to(self.q)
-
-        if q_ratio != 1.:
-            predicted_params.scale_with_q(q_ratio)
-            raw_q = raw_q * q_ratio
-            raw_q_t = raw_q_t * q_ratio
-
-        prediction_dict = {
-            "params": get_prediction_array(predicted_params),
-            "param_names": get_param_labels(
-                predicted_params.max_layer_num,
-                thickness_name='d',
-                roughness_name='sigma',
-                sld_name='rho',
-            ),
-            "curve_predicted": predicted_params.reflectivity(raw_q_t).squeeze().cpu().numpy()
-        }
-
-        sld_x_axis, sld_profile, _ = get_density_profiles(
-            predicted_params.thicknesses, predicted_params.roughnesses, predicted_params.slds, num=1024,
-        )
-
-        prediction_dict['sld_profile'] = sld_profile.squeeze().cpu().numpy()
-        prediction_dict['sld_x_axis'] = sld_x_axis.squeeze().cpu().numpy()
-
-        if polish:
-            prediction_dict.update(self._polish_prediction(
-                raw_q, raw_curve, predicted_params, priors, sld_x_axis,
-                max_d_change=max_d_change, fit_growth=fit_growth,
-            ))
-
-            if fit_growth and "params_polished" in prediction_dict:
-                prediction_dict["param_names"].append("max_d_change")
-
-        return prediction_dict
-
-    ### some shortcut methods for data processing ###
-
-    def _simple_prediction(self, scaled_curve, scaled_bounds) -> UniformSubPriorParams:
-        context = torch.cat([scaled_curve, scaled_bounds], -1)
-
-        with torch.no_grad():
-            self.trainer.model.eval()
-            scaled_params = self.trainer.model(context)
-
-        predicted_params: UniformSubPriorParams = self._restore_predicted_params(scaled_params, context)
-        return predicted_params
 
     @print_time
     def _qshift_prediction(self, curve, scaled_bounds, num: int = 1000, dq_coef: float = 1.) -> UniformSubPriorParams:
@@ -320,7 +154,7 @@ class InferenceModel(object):
                 self._get_likelihood(curve),
             )
             return best_param
-
+        
     @print_time
     def _polish_prediction(self,
                            q: np.ndarray,
@@ -365,86 +199,26 @@ class InferenceModel(object):
         polished_params_dict['sld_profile_polished'] = sld_profile_polished.squeeze().cpu().numpy()
 
         return polished_params_dict
-
-    def _restore_predicted_params(self, scaled_params: Tensor, context: Tensor) -> UniformSubPriorParams:
-        predicted_params: UniformSubPriorParams = self.trainer.loader.prior_sampler.restore_params(
-            self.trainer.loader.prior_sampler.PARAM_CLS.restore_params_from_context(scaled_params, context)
-        )
-        return predicted_params
-
-    def _input2context(self, curve: np.ndarray, priors: np.ndarray, q_ratio: float = 1.):
-        scaled_curve = self._scale_curve(curve)
-        scaled_bounds, min_bounds, max_bounds = self._scale_priors(priors, q_ratio)
-        scaled_input = torch.cat([scaled_curve, scaled_bounds], -1)
-        return scaled_input, min_bounds, max_bounds
+    
 
     def _scale_curve(self, curve: np.ndarray or Tensor):
         if not isinstance(curve, Tensor):
             curve = torch.from_numpy(curve).float()
-        curve = torch.atleast_2d(curve).to(self.q)
+        curve = torch.atleast_2d(curve).to(self.device)
         scaled_curve = self.trainer.loader.curves_scaler.scale(curve)
-        return scaled_curve.float()
+        return scaled_curve
+    
+    def _scale_prior_bounds(self, prior_bounds: List[Tuple]):
+        prior_bounds = torch.tensor(prior_bounds)
+        prior_bounds = prior_bounds.to(self.device).T
+        min_bounds, max_bounds = prior_bounds[:, None]
 
-    def _scale_priors(self, priors: np.ndarray or Tensor, q_ratio: float = 1.):
-        if not isinstance(priors, Tensor):
-            priors = torch.from_numpy(priors)
-
-        priors = priors.float().clone()
-
-        priors = priors.to(self.q).T
-        priors = self._prior_sampler.scale_bounds_with_q(priors, 1 / q_ratio)
-        priors = self._prior_sampler.clamp_bounds(priors)
-
-        min_bounds, max_bounds = priors[:, None].to(self.q)
-        prior_sampler = self._prior_sampler
         scaled_bounds = torch.cat([
-            prior_sampler.scale_bounds(min_bounds), prior_sampler.scale_bounds(max_bounds)
+            self.trainer.loader.prior_sampler.scale_bounds(min_bounds), 
+            self.trainer.loader.prior_sampler.scale_bounds(max_bounds)
         ], -1)
-        return scaled_bounds.float(), min_bounds, max_bounds
 
-    @property
-    def _prior_sampler(self) -> ExpUniformSubPriorSampler:
-        return self.trainer.loader.prior_sampler
-
-    def _set_trainer(self, trainer, preprocessing_parameters: dict = None):
-        self.trainer = trainer
-        self.trainer.model.eval()
-        self._update_preprocessing(preprocessing_parameters)
-
-    def _update_preprocessing(self, preprocessing_parameters: dict = None):
-        self.log.debug(f"setting preprocessing_parameters {preprocessing_parameters}.")
-        self.q = self.trainer.loader.q_generator.q
-        self.preprocessing = StandardPreprocessing(
-            self.q.cpu().squeeze().numpy(),
-            **(preprocessing_parameters or {})
-        )
-        self.log.info(f"preprocessing params are set: {preprocessing_parameters}.")
-
-    @print_time
-    def _sampler_solution(
-            self,
-            curve: Tensor or np.ndarray,
-            predicted_params: UniformSubPriorParams,
-    ) -> UniformSubPriorParams:
-
-        if not isinstance(curve, Tensor):
-            curve = torch.from_numpy(curve).float()
-        curve = curve.to(self.q)
-
-        refined_params = simple_sampler_solution(
-            self._get_likelihood(curve),
-            predicted_params,
-            self._prior_sampler.min_bounds,
-            self._prior_sampler.max_bounds,
-            num=self._sampling_num, coef=0.1,
-        )
-
-        return refined_params
-
-    def _get_likelihood(self, curve, rel_err: float = 0.1, abs_err: float = 1e-12):
-        return LogLikelihood(
-            self.q, curve, self._prior_sampler, curve * rel_err + abs_err
-        )
+        return scaled_bounds.float()
 
 
 def get_prediction_array(params: Params) -> np.ndarray:
