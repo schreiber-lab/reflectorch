aaa492448dee6599ad2cd813ae5b14a556295b20
Valentin Munteanu <60343516+valentinsingularity@users.noreply.github.com>
2026-04-02 10:50:11 +0200

    added sigmas and zero_init options

--- reflectorch/models/networks/mlp_networks.py
+++ reflectorch/models/networks/mlp_networks.py
@@ -55,7 +55,9 @@ class NetworkWithPriors(nn.Module):
                  residual: bool = True,
                  adaptive_activation: bool = False,
                  conditioning: str = 'concat',
-                 concat_condition_first_layer: bool = True):
+                 concat_condition_first_layer: bool = True,
+                 zero_init_last: bool = False,
+                 zero_init_blocks: bool = False):
         super().__init__()
 
         self.conditioning = conditioning
@@ -100,6 +102,8 @@ class NetworkWithPriors(nn.Module):
             adaptive_activation=adaptive_activation,
             conditioning=conditioning,
             concat_condition_first_layer=concat_condition_first_layer,
+            zero_init_last=zero_init_last,
+            zero_init_blocks=zero_init_blocks,
         )
 
         if use_selu_init and embedding_net_kwargs.get('activation', None) == 'selu':
@@ -111,12 +115,13 @@ class NetworkWithPriors(nn.Module):
             self.embedding_net.load_weights(pretrained_embedding_net)
 
 
-    def forward(self, curves, bounds, q_values=None, conditioning_params=None, key_padding_mask=None, unscaled_q_values=None):
+    def forward(self, curves, bounds, q_values=None, sigmas=None, conditioning_params=None, key_padding_mask=None, unscaled_q_values=None):
         """
         Args:
             scaled_curves (torch.Tensor): Input tensor of shape [batch_size, n_points] or [batch_size, n_channels, n_points].
             scaled_bounds (torch.Tensor): Tensor representing prior bounds, shape [batch_size, 2*n_params].
             scaled_q_values (torch.Tensor, optional): Tensor of shape [batch_size, n_points].
+            scaled_sigmas (torch.Tensor, optional): Tensor of shape [batch_size, n_points].
             scaled_conditioning_params (torch.Tensor, optional): Additional parameters for conditioning, shape [batch_size, ...].
         """
 
@@ -126,6 +131,8 @@ class NetworkWithPriors(nn.Module):
         additional_channels = []
         if q_values is not None and not isinstance(self.embedding_net, IntegralConvEmbedding):
             additional_channels.append(q_values.unsqueeze(1))
+        if sigmas is not None:
+            additional_channels.append(sigmas.unsqueeze(1))
 
         if additional_channels:
             curves = torch.cat([curves] + additional_channels, dim=1)  # [batch_size, n_channels, n_points]
@@ -187,240 +194,6 @@ class NetworkWithPriorsFnoEmb(NetworkWithPriors):
             embedding_net_kwargs=embedding_net_kwargs,
             **kwargs
         )
-
-# class NetworkWithPriorsConvEmb(nn.Module):
-#     """MLP network with 1D CNN embedding network
-
-#     .. image:: ../documentation/FigureReflectometryNetwork.png
-#         :width: 800px
-#         :align: center
-
-#     Args:
-#         in_channels (int, optional): the number of input channels of the 1D CNN. Defaults to 1.
-#         hidden_channels (tuple, optional): list with the number of channels for each layer of the 1D CNN. Defaults to (32, 64, 128, 256, 512).
-#         dim_embedding (int, optional): the dimension of the embedding produced by the 1D CNN. Defaults to 128.
-#         dim_avpool (int, optional): the type of activation function in the 1D CNN. Defaults to 1.
-#         embedding_net_activation (str, optional): the type of activation function in the 1D CNN. Defaults to 'gelu'.
-#         use_batch_norm (bool, optional): whether to use batch normalization (in both the 1D CNN and the MLP). Defaults to False.
-#         dim_out (int, optional): the dimension of the output produced by the MLP. Defaults to 8.
-#         layer_width (int, optional): the width of a linear layer in the MLP. Defaults to 512.
-#         num_blocks (int, optional): the number of residual blocks in the MLP. Defaults to 4.
-#         repeats_per_block (int, optional): the number of normalization/activation/linear repeats in a block. Defaults to 2.
-#         mlp_activation (str, optional):  the type of activation function in the MLP. Defaults to 'gelu'.
-#         dropout_rate (float, optional): dropout rate for each block. Defaults to 0.0.
-#         use_selu_init (bool, optional): whether to use the special weights initialization for the 'selu' activation function. Defaults to False.
-#         pretrained_embedding_net (str, optional): the path to the weights of a pretrained embedding network. Defaults to None.
-#         residual (bool, optional): whether the blocks have a residual skip connection. Defaults to True.
-#         adaptive_activation (bool, optional): must be set to ``True`` if the activation function is adaptive. Defaults to False.
-#         conditioning (str, optional): the manner in which the prior bounds are provided as input to the network. Defaults to 'concat'.
-#     """
-#     def __init__(self,
-#                  in_channels: int = 1,
-#                  hidden_channels: tuple = (32, 64, 128, 256, 512),
-#                  dim_embedding: int = 128,
-#                  dim_avpool: int = 1,
-#                  embedding_net_activation: str = 'gelu',
-#                  use_batch_norm: bool = False,
-#                  dim_out: int = 8,
-#                  layer_width: int = 512,
-#                  num_blocks: int = 4,
-#                  repeats_per_block: int = 2,
-#                  mlp_activation: str = 'gelu',
-#                  dropout_rate: float = 0.0,
-#                  use_selu_init: bool = False,
-#                  pretrained_embedding_net: str = None,
-#                  residual: bool = True,
-#                  adaptive_activation: bool = False,
-#                  conditioning: str = 'concat',
-#                  ):
-#         super().__init__()
-
-#         self.in_channels = in_channels
-#         self.conditioning = conditioning
-
-#         self.embedding_net = ConvEncoder(
-#             in_channels=in_channels, 
-#             hidden_channels=hidden_channels, 
-#             dim_latent=dim_embedding, 
-#             dim_avpool=dim_avpool,
-#             use_batch_norm=use_batch_norm, 
-#             activation=embedding_net_activation
-#         )
-        
-#         self.dim_prior_bounds = 2 * dim_out
-
-#         if conditioning == 'concat':
-#             dim_mlp_in = dim_embedding + self.dim_prior_bounds
-#             dim_condition = 0
-#         elif conditioning == 'glu' or conditioning == 'film':
-#             dim_mlp_in = dim_embedding
-#             dim_condition = self.dim_prior_bounds
-#         else:
-#             raise NotImplementedError
-
-#         self.mlp = ResidualMLP(
-#             dim_in=dim_mlp_in,
-#             dim_out=dim_out,
-#             dim_condition=dim_condition,
-#             layer_width=layer_width,
-#             num_blocks=num_blocks,
-#             repeats_per_block=repeats_per_block,
-#             activation=mlp_activation,
-#             use_batch_norm=use_batch_norm,
-#             dropout_rate=dropout_rate,
-#             residual=residual,
-#             adaptive_activation=adaptive_activation,
-#             conditioning=conditioning,
-#         )
-
-#         if use_selu_init and embedding_net_activation == 'selu':
-#             self.embedding_net.apply(selu_init)
-
-#         if use_selu_init and mlp_activation == 'selu':
-#             self.mlp.apply(selu_init)
-
-#         if pretrained_embedding_net:
-#             self.embedding_net.load_weights(pretrained_embedding_net)
-
-
-#     def forward(self, curves: Tensor, bounds: Tensor, q_values: Optional[Tensor] = None):
-#         """
-#         Args:
-#             curves (Tensor): reflectivity curves
-#             bounds (Tensor): prior bounds
-#             q_values (Tensor, optional): q values. Defaults to None.
-
-#         Returns:
-#             Tensor: prediction
-#         """
-#         if q_values is not None:
-#             curves = torch.cat([curves[:, None, :], q_values[:, None, :]], dim=1)
-
-#         if self.conditioning == 'concat': 
-#             x = torch.cat([self.embedding_net(curves), bounds], dim=-1)
-#             x = self.mlp(x)
-
-#         elif self.conditioning == 'glu' or self.conditioning == 'film':
-#             x = self.mlp(self.embedding_net(curves), condition=bounds)
-
-#         return x
-    
-    
-# class NetworkWithPriorsFnoEmb(nn.Module):
-#     """MLP network with FNO embedding network
-
-#     Args:
-#         in_channels (int, optional): the number of input channels to the FNO-based embedding network. Defaults to 2.
-#         dim_embedding (int, optional): the dimension of the embedding produced by the FNO. Defaults to 128.
-#         modes (int, optional):  the number of Fourier modes that are utilized. Defaults to 16.
-#         width_fno (int, optional): the number of channels in the FNO blocks. Defaults to 64.
-#         embedding_net_activation (str, optional):  the type of activation function in the embedding network. Defaults to 'gelu'.
-#         n_fno_blocks (int, optional): the number of FNO blocks. Defaults to 6.
-#         fusion_self_attention (bool, optional): if ``True`` a fusion layer is used after the FNO blocks to produce the final output. Defaults to False.
-#         dim_out (int, optional): the dimension of the output produced by the MLP. Defaults to 8.
-#         layer_width (int, optional): the width of a linear layer in the MLP. Defaults to 512.
-#         num_blocks (int, optional): the number of residual blocks in the MLP. Defaults to 4.
-#         repeats_per_block (int, optional): the number of normalization/activation/linear repeats in a block. Defaults to 2.
-#         use_batch_norm (bool, optional): whether to use batch normalization (only in the MLP). Defaults to False.
-#         mlp_activation (str, optional):  the type of activation function in the MLP. Defaults to 'gelu'.
-#         dropout_rate (float, optional): dropout rate for each block. Defaults to 0.0.
-#         use_selu_init (bool, optional): whether to use the special weights initialization for the 'selu' activation function. Defaults to False.
-#         residual (bool, optional): whether the blocks have a residual skip connection. Defaults to True.
-#         adaptive_activation (bool, optional): must be set to ``True`` if the activation function is adaptive. Defaults to False.
-#         conditioning (str, optional): the manner in which the prior bounds are provided as input to the network. Defaults to 'concat'.
-#     """
-#     def __init__(self,
-#                  in_channels: int = 2,
-#                  dim_embedding: int = 128,
-#                  modes: int = 16,
-#                  width_fno: int = 64,
-#                  embedding_net_activation: str = 'gelu',
-#                  n_fno_blocks : int = 6,
-#                  fusion_self_attention: bool = False,
-#                  dim_out: int = 8,
-#                  layer_width: int = 512,
-#                  num_blocks: int = 4,
-#                  repeats_per_block: int = 2,
-#                  use_batch_norm: bool = False,
-#                  mlp_activation: str = 'gelu',
-#                  dropout_rate: float = 0.0,
-#                  use_selu_init: bool = False,
-#                  residual: bool = True,
-#                  adaptive_activation: bool = False,
-#                  conditioning: str = 'concat',
-#                  ):
-#         super().__init__()
-
-#         self.conditioning = conditioning
-
-#         self.embedding_net = FnoEncoder(
-#             ch_in=in_channels, 
-#             dim_embedding=dim_embedding, 
-#             modes=modes, 
-#             width_fno=width_fno, 
-#             n_fno_blocks=n_fno_blocks, 
-#             activation=embedding_net_activation,
-#             fusion_self_attention=fusion_self_attention
-#         )
-
-#         self.dim_prior_bounds = 2 * dim_out
-
-#         if conditioning == 'concat':
-#             dim_mlp_in = dim_embedding + self.dim_prior_bounds
-#             dim_condition = 0
-#         elif conditioning == 'glu' or conditioning == 'film':
-#             dim_mlp_in = dim_embedding
-#             dim_condition = self.dim_prior_bounds
-#         else:
-#             raise NotImplementedError
-
-#         self.mlp = ResidualMLP(
-#             dim_in=dim_mlp_in,
-#             dim_out=dim_out,
-#             dim_condition=dim_condition,
-#             layer_width=layer_width,
-#             num_blocks=num_blocks,
-#             repeats_per_block=repeats_per_block,
-#             activation=mlp_activation,
-#             use_batch_norm=use_batch_norm,
-#             dropout_rate=dropout_rate,
-#             residual=residual,
-#             adaptive_activation=adaptive_activation,
-#             conditioning=conditioning,
-#         )
-
-#         if use_selu_init and embedding_net_activation == 'selu':
-#             self.FnoEncoder.apply(selu_init)
-
-#         if use_selu_init and mlp_activation == 'selu':
-#             self.mlp.apply(selu_init)
-
-            
-#     def forward(self, curves: Tensor, bounds: Tensor, q_values: Optional[Tensor] =None):
-#         """
-#         Args:
-#             curves (Tensor): reflectivity curves
-#             bounds (Tensor): prior bounds
-#             q_values (Tensor, optional): q values. Defaults to None.
-
-#         Returns:
-#             Tensor: prediction
-#         """
-#         if curves.dim() < 3:
-#             curves = curves[:, None, :]
-#         if q_values is not None:
-#             curves = torch.cat([curves, q_values[:, None, :]], dim=1)
-
-#         if self.conditioning == 'concat': 
-#             x = torch.cat([self.embedding_net(curves), bounds], dim=-1)
-#             x = self.mlp(x)
-
-#         elif self.conditioning == 'glu' or self.conditioning == 'film':
-#             x = self.mlp(self.embedding_net(curves), condition=bounds)
-
-#         return x
-
-
 
 def selu_init(m):
     if isinstance(m, (nn.Conv1d, nn.Linear)):
